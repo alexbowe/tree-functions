6f4b930797e47a7fc3c2c102a04c753983562fae
alexbowe <bowe.alexander@gmail.com>
2012-03-16 15:25:00 +0900

    working implementation, at least on host side

--- PseudoTree.h
+++ PseudoTree.h
@@ -6,21 +6,18 @@ namespace tree_functions
 {
 
 // TODO: look at alignment, maybe use cuda small vector types
-template<typename T, typename AssociativeOperator, typename UnaryOperator, T ID1, T ID2>
+template<typename T, typename AssociativeOperator1, typename AssociativeOperator2, typename UnaryOperator, T ID1, T ID2>
 class PseudoTree
 {
 public:
-    // static member classes passed as templates?
-    static AssociativeOperator op1;
-    static AssociativeOperator op2;
-    static UnaryOperator inv;
-
-
+    // Default Constructor - An identity pseudotree for this operation
+    #ifdef __CUDACC__
     __device__ __host__
+    #endif
     PseudoTree() : L(0), M(0), B(0), E(0), R(ID1), A(ID1), F(ID2) { ; }
 
-    __device__ __host__
     // ESSENTIAL that parenth doesnt take a value other than 0 or 1
+    __device__ __host__
     PseudoTree(bool parenth, T weight) :
     	// ( -> (1,   0,  1,  1, w,   w, id2)
         // ) -> (-1, -1, -1, -1, w, id1, id2)
@@ -33,24 +30,30 @@ public:
         F(ID2)
         { ; }
     
-    // templated constructor with block type parameter that adds them sequentially?
+    // TODO: templated constructor with block type parameter that adds them sequentially?
+    #ifdef __CUDACC__
     __device__ __host__
-    template <typename AssociativeOperator, typename UnaryOperator>
-    PseudoTree merge(const PseudoTree& a, const PseudoTree& b, AssociativeOperator op1, AssociativeOperator op2, UnaryOperator inv)
+    #endif
+    static
+    PseudoTree merge(const PseudoTree& a, const PseudoTree& b)
     {
         int L = a.L + b.L;
-        int M = min(a.M, b.M);
+        int M = min(a.M, a.L + b.M);
         int B = a.B? a.B : b.B;
         int E = b.E? b.E : a.E;
-        T R = op1(a.R, b.R);
-        T A = (M == a.M)? op1(a.A, b.R) : b.A;
-        T F_prime = 0;//(E == 1 && b.B == -1)? OP2(F, A) : F;
-        T H = 0;//OP1(OP1(A, b.R), Inv1(b.A));
-        T new_F = 0;//(new_M == M)? OP2(F_prime, OP1(H, b.F)) : OP2(OP1(Inv1(H), F_prime), b.F);
-        return PseudoTree(new_L, new_M, new_B, new_E, new_R, new_A, new_F);
+        T   R = op1(a.R, b.R);
+        T   A = (M == a.M)? op1(a.A, b.R) : b.A;
+        T   F_prime = (a.E == 1 && b.B == -1)? op2(a.F, a.A) : a.F;
+        T   H = op1(op1(a.A, b.R), inv(b.A));
+        T   F = (M == a.M)? op2(F_prime, op1(H, b.F)) : op2(op1(inv(H), F_prime), b.F);
+        return PseudoTree(L, M, B, E, R, A, F);
     }
 
-//private:
+//protected:
+    static AssociativeOperator1 op1;
+    static AssociativeOperator2 op2;
+    static UnaryOperator inv;
+
     // could use cuda int4 here?
     int L, M, B, E;
     T R, A, F;
@@ -58,6 +61,16 @@ public:
     __device__ __host__
     PseudoTree(int l, int m, int b, int e, T r, T a, T f) : L(l), M(m), B(b), E(e), R(r), A(a), F(f) { ; }
 };
+
+// Initialise templated operators
+template<typename T, typename AssociativeOperator1, typename AssociativeOperator2, typename UnaryOperator, T ID1, T ID2>
+AssociativeOperator1 PseudoTree<T, AssociativeOperator1, AssociativeOperator2, UnaryOperator, ID1, ID2>::op1 = AssociativeOperator1();
+
+template<typename T, typename AssociativeOperator1, typename AssociativeOperator2, typename UnaryOperator, T ID1, T ID2>
+AssociativeOperator2 PseudoTree<T, AssociativeOperator1, AssociativeOperator2, UnaryOperator, ID1, ID2>::op2 = AssociativeOperator2();
+
+template<typename T, typename AssociativeOperator1, typename AssociativeOperator2, typename UnaryOperator, T ID1, T ID2>
+UnaryOperator PseudoTree<T, AssociativeOperator1, AssociativeOperator2, UnaryOperator, ID1, ID2>::inv = UnaryOperator();
 
 }
 #endif
--- main.cu
+++ main.cu
@@ -20,29 +20,28 @@ void print_pt(PT pt)
     printf("( %d, %d, %d, %d, %d, %d, %d )\n", pt.L, pt.M, pt.B, pt.E, pt.R, pt.A, pt.F);
 }
 
+// This defines the tree function for the "Maximum Path" problem
+// Others are possible, and may be provided in a library later.
+typedef int node_value_t;
+typedef thrust::plus<node_value_t> op1;
+typedef thrust::maximum<node_value_t> op2;
+typedef thrust::negate<node_value_t> inv;
+const int id1 = 0;
+const int id2 = INT_MIN;
+typedef tree_functions::PseudoTree<node_value_t, op1, op2, inv, id1, id2> ptree;
+
 int main(void)
 {
-    typedef int node_value_t;
-    typedef thrust::plus<node_value_t> op1;
-    typedef thrust::maximum<node_value_t> op2;
-    typedef thrust::negate<node_value_t> inv1;
-    const int id1 = 0;
-    const int id2 = INT_MIN;
-
-    op1 plus;
-    op2 mx;
-    inv1 neg;
-    typedef tree_functions::PseudoTree<node_value_t, op1, op2, neg, id1, id2> ptree;
-
     // TODO: Use a transformation mapper with a reduce? reduce is associative
     // if it isnt associative, use a scan
     // this way sequentialness is handled by thrust...
     // then make a mapping function that does the sequential creation of a bitmap block
-    printf("%d\n", plus(2, 3));
-    //ptree a(0,5);
-    //ptree b(1, 7);
-    //print_pt<ptree>(a);
-    //print_pt<ptree>(b);
-    //print_pt<ptree>(a + b);
+    ptree a(0, 5);
+    ptree b(1, 7);
+    print_pt<ptree>(a);
+    print_pt<ptree>(b);
+    print_pt<ptree>(ptree::merge(a, b));
+    int excess = sizeof(ptree) - 3*sizeof(node_value_t) - 4*sizeof(int);
+    printf("excess: %d\n", excess);
     return 0;
 }
